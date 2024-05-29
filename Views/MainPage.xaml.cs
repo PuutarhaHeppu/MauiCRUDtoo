2d6aac5cdef88eaaf828b300a3ed35ef3e8bfe7a
Opilane TTHK <opilane@tthk.ee>
2024-05-29 10:41:54 +0300

    miiks

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -14,9 +14,37 @@ namespace MauiCRUDtoo
             Task.Run(async () => listView.ItemsSource = await _dbService.GetCustomers());
         }
 
-        private void saveButton_Clicked(object sender, EventArgs e)
+        private async void saveButton_Clicked(object sender, EventArgs e)
         {
-            if 
+            if (_editCustomerId == 0)
+            {
+                await _dbService.Create(new Customer
+                {
+                    CustomerName = nameEntryField.Text,
+                    Email = emailEntryField.Text,
+                    Mobile = mobileEntryField.Text
+                });
+            }
+            else
+            {
+                // Edit Customer
+
+                await _dbService.Update(new Customer
+                {
+                    Id = _editCustomerId,
+                    CustomerName = nameEntryField.Text,
+                    Email = emailEntryField.Text,
+                    Mobile = mobileEntryField.Text
+                });
+
+                _editCustomerId = 0;
+            }
+
+            nameEntryField.Text = string.Empty;
+            emailEntryField.Text = string.Empty;
+            mobileEntryField.Text = string.Empty;
+
+            listView.ItemsSource = await _dbService.GetCustomers();
         }
 
         private void listView_ItemTapped(object sender, ItemTappedEventArgs e)
